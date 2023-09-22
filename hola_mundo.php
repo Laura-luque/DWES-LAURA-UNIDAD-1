ad5331bb361c629181eab865bf09caddc3dbbf2a
Laura-luque <115107880+Laura-luque@users.noreply.github.com>
2023-09-22 10:29:24 +0200

    Update hola_mundo.php

--- hola_mundo.php
+++ hola_mundo.php
@@ -1,4 +1,4 @@
 <?php
-    echo 'Hola mundo';
-    echo "<a href='https://github.com/Laura-luque/DWES-LAURA'>Ver código</a>";
+    echo 'Hola mundo'; # Mostramos por pantalla el texto entrecomillado
+    echo "<a href='https://github.com/Laura-luque/DWES-LAURA'>Ver código</a>"; # Se muestra por pantalla el hipervínculo hacia el repositorio donde se encuentra el código PHP
 ?>
