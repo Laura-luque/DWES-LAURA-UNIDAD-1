0ed7f7027aa9e74c72564fedc2dec12b464064c4
Laura-luque <115107880+Laura-luque@users.noreply.github.com>
2023-09-29 21:52:29 +0200

    Add files

---
+++ Codigo_Hola_mundo/hola_mundo.php
@@ -0,0 +1,4 @@
+<?php
+    echo 'Hola mundo'; # Mostramos por pantalla el texto entrecomillado
+    echo "<a href='https://github.com/Laura-luque/DWES-LAURA'>Ver código</a>"; # Se muestra por pantalla el hipervínculo hacia el repositorio donde se encuentra el código PHP
+?>
